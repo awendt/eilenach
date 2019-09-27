0729519311b7f1eacd651d6712a5c5a947aae136
André Wendt <awendt@freeshell.org>
2019-09-27 08:57:40 +0200

    terraform 0.12upgrade

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,8 +1,23 @@
-variable "banking_blz" {}
-variable "banking_username" {}
-variable "banking_pin" {}
-variable "banking_endpoint" {}
-variable "shared_account_number" {}
-variable "mail_recipient" {}
-variable "mailgun_key" {}
-variable "mailgun_domain" {}
+variable "banking_blz" {
+}
+
+variable "banking_username" {
+}
+
+variable "banking_pin" {
+}
+
+variable "banking_endpoint" {
+}
+
+variable "shared_account_number" {
+}
+
+variable "mail_recipient" {
+}
+
+variable "mailgun_key" {
+}
+
+variable "mailgun_domain" {
+}
